b8c9170965d7b9f151d717c660e904335f2aaf92
Gugan M R <gugan.mr2024cse@sece.ac.in>
2025-01-07 14:38:46 +0530

    Add files via upload

---
+++ function prototype.c
@@ -0,0 +1,6 @@
+// Function prototypes
+int create(); // Function to create a new team
+int hire(); // Function to hire team members
+int assign(); // Function to assign a project to a team
+int fire(); // Function to fire a team member
+int display(); // Function to display team details
